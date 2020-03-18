0a8267a05e2deb9b323e4cb6a67b2fa809019902
KJSain <kip.jay@phynix.io>
2020-03-17 19:02:50 -0600

    added gfac and password change for db

---
+++ config-lims-database/files/gfac-db.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: 
+-- ------------------------------------------------------
+-- Server version	5.1.73-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `gfac`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `gfac` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `gfac`;
+
+--
+-- Table structure for table `analysis`
+--
+
+DROP TABLE IF EXISTS `analysis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `analysis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gfacID` varchar(80) DEFAULT NULL,
+  `cluster` varchar(64) DEFAULT NULL,
+  `us3_db` varchar(32) DEFAULT NULL,
+  `stdout` longtext,
+  `stderr` longtext,
+  `tarfile` mediumblob,
+  `status` enum('SUBMITTED','SUBMIT_TIMEOUT','RUNNING','RUN_TIMEOUT','DATA','DATA_TIMEOUT','COMPLETE','CANCELLED','CANCELED','FAILED','FAILED_DATA','ERROR') DEFAULT 'SUBMITTED',
+  `queue_msg` text,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=414 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cluster_status`
+--
+
+DROP TABLE IF EXISTS `cluster_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cluster_status` (
+  `cluster` varchar(120) NOT NULL,
+  `queued` int(11) DEFAULT NULL,
+  `running` int(11) DEFAULT NULL,
+  `status` enum('up','down','warn','unknown') DEFAULT 'up',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `queue_messages`
+--
+
+DROP TABLE IF EXISTS `queue_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `queue_messages` (
+  `messageID` int(11) NOT NULL AUTO_INCREMENT,
+  `analysisID` int(11) NOT NULL,
+  `message` text,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`messageID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5287341 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
